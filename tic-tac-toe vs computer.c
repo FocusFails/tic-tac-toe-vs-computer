57beb33e5d4caa841a0534ae2d45650635be14ee
FocusFails <162300415+FocusFails@users.noreply.github.com>
2026-03-13 03:12:42 +0545

    Update tic-tac-toe vs computer.c
    mistake in checkWinner( )

--- tic-tac-toe vs computer.c
+++ tic-tac-toe vs computer.c
@@ -222,19 +222,19 @@ char checkWinner(char board[3][3]) {
     for(i=0;i<3;i++) {
         //Checking columns for win
         if (board[i][0] != ' ' && board[i][1] == board[i][0] && board[i][1] == board[i][2]) {
-                if (board[i][1] == 'O') return board[i][0];
+                return board[i][0];
         }
         //Checking rows for win
         if(board[0][i] != ' ' && board[1][i] == board[0][i] && board[1][i] == board[2][i]) {
-                if (board[i][1] == 'O') return board[0][i];
+                return board[0][i];
         }
         }
     // Checking diagonals for win
     if (board[1][1]!= ' ' && board[1][1] == board[2][2] && board[1][1] == board[0][0]) {
-            if (board[1][1] == 'O') return board[1][1];
+            return board[1][1];
     }
     if (board[1][1]!= ' ' && board[1][1] == board[2][0] && board[1][1] == board[0][2]) {
-            if (board[1][1] == 'O') return board[1][1];
+            return board[1][1];
     }
     // Nope, continue playing
     return 'N';
